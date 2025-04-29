0c9c21ba18cf2b373d5fc1d3c66fbf0cd99d06e9
guilhermewqz <gui7sampaio@gmail.com>
2025-04-28 18:18:52 -0700

    segundo exemplo

---
+++ exemplo2/main.js
@@ -0,0 +1,11 @@
+function somar(numero1, numero2) {
+    console.log(numero1 + numero2);
+  }
+  
+  let numero1 = 40;
+  let numero2 = 23;
+  
+  somar(numero1, numero2);
+  somar(30, 12);
+  somar(19, 289);
+  
